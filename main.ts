719cfc273a9f7e9452a2cbde1c0a9f49d92e1c7c
ocantonio <122565417+ocantonio@users.noreply.github.com>
2023-11-02 21:22:51 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
 basic.forever(function () {
-	
+    for (let y = 0; y <= 4; y++) {
+        led.plot(0, y)
+        basic.pause(100)
+        led.unplot(0, y)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(2, y)
+        basic.pause(100)
+        led.unplot(2, y)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(4, y)
+        basic.pause(100)
+        led.unplot(4, y)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(1, 4 - y)
+        basic.pause(100)
+        led.unplot(1, 4 - y)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
+    for (let y = 0; y <= 4; y++) {
+        led.plot(3, 4 - y)
+        basic.pause(100)
+        led.unplot(3, 4 - y)
+        basic.pause(100)
+    }
 })
